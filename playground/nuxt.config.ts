3d22efe8f9b36f3e171265cef29a8d9f76bc84ab
Arash <38922203+arashsheyda@users.noreply.github.com>
2023-11-05 11:05:05 -0700

    chore

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,10 +2,10 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
 
   paypal: {
-    clientId: 'ASgxdor6DbG1fFxE68jxmZia_eERj7tOr1_kZHcUTu6zzFMU-N2qqNQMx2zCXkwphZATN8Pt-QGZHeDO',
+    // clientId: '',
   },
 
-  // devtools: {
-  //   enabled: true,
-  // },
+  devtools: {
+    enabled: true,
+  },
 })
